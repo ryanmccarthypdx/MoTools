46b4287ea96d95aa0942c147f5838916433bf23f
Ryan <ryanmccarthypdx@gmail.com>
2015-03-09 11:32:14 -0700

    Add first migration, setting up internships table

--- db/migrate/20150309181300_create_internships.rb
+++ db/migrate/20150309181300_create_internships.rb
@@ -1,4 +1,21 @@
 class CreateInternships < ActiveRecord::Migration
   def change
+    create_table(:internships) do |t|
+      t.string :company_name
+      t.string :contact_name
+      t.string :contact_phone
+      t.string :contact_email
+      t.string :company_website
+      t.string :company_address
+      t.string :company_description
+      t.string :intern_work
+      t.string :intern_ideal
+      t.string :intern_count
+      t.boolean :intern_clearance
+      t.string :intern_clearance_description
+      t.string :mentor_name
+      t.string :mentor_email
+      t.string :mentor_phone
+    end
   end
 end
