98a13bdac8bf253a01f8a1287da7b8e0d6b599ef
Ryan <ryanmccarthypdx@gmail.com>
2015-03-09 15:29:11 -0700

    Add ability to create internship

--- app.rb
+++ app.rb
@@ -4,5 +4,38 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get '/' do
-  erb :index
+  erb :new_internship
+end
+
+get '/internships' do
+  @internships = Internship.all()
+  erb :internships
+end
+
+post '/internships' do
+  internship = Internship.create({
+    :company_name => params.fetch("company_name"),
+    :contact_name => params.fetch("contact_name"),
+    :contact_phone => params.fetch("contact_phone"),
+    :contact_email => params.fetch("contact_email"),
+    :company_website => params.fetch("company_website"),
+    :company_address => params.fetch("company_address"),
+    :company_description => params.fetch("company_description"),
+    :intern_work => params.fetch("intern_work"),
+    :intern_ideal => params.fetch("intern_ideal"),
+    :intern_count => params.fetch("intern_count"),
+    :intern_clearance => params.fetch("intern_clearance"),
+    :intern_clearance_description => params.fetch("intern_clearance_description"),
+    :mentor_name => params.fetch("mentor_name"),
+    :mentor_email => params.fetch("mentor_email"),
+    :mentor_phone => params.fetch("mentor_phone")
+  })
+  @internship_id = internship.id()
+  redirect "/internships/#{@internship_id}"
+end
+
+get '/internships/:internship_id' do
+  internship_id = params.fetch('internship_id')
+  @internship = Internship.find(internship_id)
+  erb :internship
 end
