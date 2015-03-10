aabf892f4ddc79792770a471e126c8fd71116f53
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-10 13:48:17 -0700

    Add Stu Dent to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,26 @@ Internship.create({
   :mentor_email => "imajean@sample.com",
   :mentor_phone => "503-867-5309"
   })
+
+Internship.create({
+  :company_name => "FakeApps, LLC",
+  :contact_name => "Ima Contact",
+  :contact_phone => "503-555-1234",
+  :contact_email => "ima@sample.com",
+  :company_website => "www.sample.com",
+  :company_address => "123 SW Fake Street",
+  :company_description => "At FakeApps Software, we make fake apps for stupid people.  We care about fakeapps, that's why we care about idiots.  FakeApps is an awesome place to work, with 18 hour days, 6 days a week with optional SUNDAY FUNDAY code sessions that are absolutely mandatory.",
+  :intern_work => "Interns will work day and night in the code mines, battling code rats and the ever present fear of a code gas explosion.  This internship is paid, but only in FakeApp company scrip",
+  :intern_ideal => "Our ideal intern will be pliable and able to lift heavy code",
+  :intern_count => "2",
+  :intern_clearance => true,
+  :intern_clearance_description => "Pass a Fake Drug Test",
+  :mentor_name => "Imajean Bosslady",
+  :mentor_email => "imajean@sample.com",
+  :mentor_phone => "503-867-5309"
+  })
+
+
+Student.create({
+  :name => "Stu Dent"  
+  })
