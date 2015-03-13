4b6ef743a0458f025f0d6e31609c596b4ff432dd
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-03-13 15:39:31 -0700

    Add functional csv-based output.  BAM

--- lib/user_student.rb
+++ lib/user_student.rb
@@ -2,6 +2,34 @@ class Student < User
   has_many :ratings
   has_many :internships, :through => :ratings
 
+  def self.internship_data_export
+    output_array = []
+    header_row = ["Student Name"]
+    sorted_internships = Internship.all.sort_by { |internship| internship.id }
+    sorted_internships.each do |internship|
+      header_row.push(internship.company_name)
+    end
+    output_array.push(header_row)
+    Student.all.each do |student|
+      student_row = [student.name]
+      sorted_internships.each do |internship|
+        if student.top_ten.include?(internship)
+          student_row.push(student.top_ten.index(internship) + 1)
+        else
+          student_row.push(nil)
+        end
+      end
+      output_array.push(student_row)
+    end
+
+    CSV.open("public/output.csv", "wb") do |csv|
+      output_array.each() do |student_row|
+        csv << student_row
+      end
+    end
+
+  end
+
   def sorted_ratings
     ratings.sort_by {|rating| rating.total_rating}.reverse
   end
---
+++ spec/list_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe List do
+  describe("#save_top_ten") do
+    it("finds a user's top ten list and saves it to array master_array") do
+
+    end
+  end
+end
